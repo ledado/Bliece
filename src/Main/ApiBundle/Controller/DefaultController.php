70ac27129668d0903c033839e37c6de8ad5202af
Andrej Lednický <lednicky.andrej@gmail.com>
2014-10-30 08:54:41 +0100

    Bliece

--- src/Main/ApiBundle/Controller/DefaultController.php
+++ src/Main/ApiBundle/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+    
     public function indexAction()
     {
         return $this->render('MainApiBundle:Default:index.html.twig');
