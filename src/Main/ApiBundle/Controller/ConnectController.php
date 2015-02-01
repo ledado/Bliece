74e970961dd03dfe3a79bc595184d705ed3796ce
Andrej Lednický <lednicky.andrej@gmail.com>
2015-02-01 08:40:25 +0100

    Pridany controller na ajaxove connectovanie userov

---
+++ src/Main/ApiBundle/Controller/ConnectController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lednicky
+ * Date: 1.2.2015
+ * Time: 8:39
+ */
+
+namespace Main\ApiBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ConnectController extends Controller {
+
+} 
