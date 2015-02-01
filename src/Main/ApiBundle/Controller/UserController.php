e80b81aba1c3b23202d18bc5276df11c21df4096
Andrej Lednický <lednicky.andrej@gmail.com>
2015-02-01 08:36:48 +0100

    Zakladna metoda na pozretie profila

--- src/Main/ApiBundle/Controller/UserController.php
+++ src/Main/ApiBundle/Controller/UserController.php
@@ -11,5 +11,18 @@ namespace Main\ApiBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class UserController extends Controller{
+    function viewAction($userId){
+        $user = $this->get('security.context')->getToken()->getUser();
+        $em = $this->get('doctrine')->getManager();
 
+        $userProfile = $em->getRepository('MainApiBundle:User')->findOneById($userId);
+
+        if(!$userProfile){
+            $userProfile = null;
+        }
+        return $this->render('MainApiBundle:User:view.html.twig', array(
+            'user' => $user,
+            'userProfile' => $userProfile
+        ));
+    }
 } 
